d37815cca2452ff2402b44923ac6f309089a7828
Jose <jfmdev@gmail.com>
2025-06-05 16:26:51 -0300

    Improve code and add example of RPC call

--- consumer/index.js
+++ consumer/index.js
@@ -1,17 +1,55 @@
-const amqp = require('amqplib');
+import amqp from 'amqplib';
+
+const CONNECTION_ATTEMPTS = 30;
+
+async function getRabbitConnection(retry = CONNECTION_ATTEMPTS) {
+  try{
+    const connection = await amqp.connect(process.env.RABBITMQ_URL);
+    return connection;
+  }catch(err) {
+    if(retry > 0) {
+      await new Promise(resolve => setTimeout(resolve, 2_000));
+      return getRabbitConnection(--retry);
+    } else {
+      throw err
+    }
+  }
+}
+
+// REQ: These names should match the ones used by the Producer.
+const SIMPLE_QUEUE = 'test_queue';
+const RPC_QUEUE = 'rpc_queue';
 
 (async () => {
-  const connection = await amqp.connect(process.env.RABBITMQ_URL);
+  const connection = await getRabbitConnection();
   const channel = await connection.createChannel();
-  const queue = 'test_queue';
 
-  await channel.assertQueue(queue, { durable: false });
+  await channel.assertQueue(SIMPLE_QUEUE, { durable: false });
+  await channel.assertQueue(RPC_QUEUE, { durable: false });
+  channel.prefetch(1);
 
   console.log(" [*] Waiting for messages...");
-  channel.consume(queue, msg => {
+
+  // Simple messages.
+  channel.consume(SIMPLE_QUEUE, msg => {
     if (msg !== null) {
-      console.log(" [x] Received:", msg.content.toString());
+      console.log(" [x] Msg received:", msg.content.toString());
       channel.ack(msg);
     }
+  });
+
+  // RPC calls.
+  channel.consume(RPC_QUEUE, function reply(msg) {
+    const numbers = msg.content.toString().split(',');
+    const result = parseInt(numbers[0]) + parseInt(numbers[1]);
+    console.log(" [.] RPC received:", numbers);
+
+    channel.sendToQueue(
+      msg.properties.replyTo,
+      Buffer.from(result.toString()),
+      { correlationId: msg.properties.correlationId }
+    );
+
+    channel.ack(msg);
   });
 })();
--- producer/index.js
+++ producer/index.js
@@ -1,15 +1,64 @@
-const amqp = require('amqplib');
+import amqp from 'amqplib';
+
+const CONNECTION_ATTEMPTS = 30;
+
+async function getRabbitConnection(retry = CONNECTION_ATTEMPTS) {
+  try{
+    const connection = await amqp.connect(process.env.RABBITMQ_URL);
+    return connection;
+  }catch(err) {
+    if(retry > 0) {
+      await new Promise(resolve => setTimeout(resolve, 2_000));
+      return getRabbitConnection(--retry);
+    } else {
+      throw err
+    }
+  }
+}
+
+
+function generateUuid() {
+  return Math.random().toString() +
+         Math.random().toString() +
+         Math.random().toString();
+}
+
+// REQ: These names should match the ones used by the Consumer.
+const SIMPLE_QUEUE = 'test_queue';
+const RPC_QUEUE = 'rpc_queue';
 
 (async () => {
-  const connection = await amqp.connect(process.env.RABBITMQ_URL);
+  const connection = await getRabbitConnection();
   const channel = await connection.createChannel();
-  const queue = 'test_queue';
 
-  await channel.assertQueue(queue, { durable: false });
+  await channel.assertQueue(SIMPLE_QUEUE, { durable: false });
+  await channel.assertQueue(RPC_QUEUE, { durable: false });
+
+  // Simple messages.
+  setInterval(() => {
+    const msg = `Simple message at ${new Date().toISOString()}`;
+
+    channel.sendToQueue(SIMPLE_QUEUE, Buffer.from(msg));
+    console.log(" [x] Msg sent:", msg);
+  }, 4_000);
+
+  // RPC calls.
+  const correlationId = generateUuid();
+  const replyQueue = await channel.assertQueue('', { exclusive: true });
+
+  channel.consume(replyQueue.queue,
+    function(msg) {
+      if (msg.properties.correlationId == correlationId) {
+        console.log(' [.] RPC Answer:', msg.content.toString());
+      }
+    },
+    { noAck: true }
+  );
 
   setInterval(() => {
-    const msg = `Message at ${new Date().toISOString()}`;
-    channel.sendToQueue(queue, Buffer.from(msg));
-    console.log(" [x] Sent:", msg);
-  }, 2000);
+    const msg = `${Math.round(Math.random()*10)},${Math.round(Math.random()*10)}`;
+    channel.sendToQueue(RPC_QUEUE, Buffer.from(msg), { replyTo: replyQueue.queue, correlationId: correlationId });
+
+    console.log(" [.] RPC Request:", msg, `at ${new Date().toISOString()}`);
+  }, 4_000);
 })();
